b30401df38902d5fb7cbbe13d7d917355d9abc7d
dougsuh <38517374+dougsuh@users.noreply.github.com>
2023-01-04 17:53:34 +0900

    Add files via upload

---
+++ lab1/lab1size.cpp
@@ -0,0 +1,11 @@
+#include <iostream>      // for cout
+using namespace std;    // for the names used by C++ (include, main, cout, endl…) 
+void main() {
+    // sizes of variable types
+    cout << sizeof(bool) << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(short) << endl;
+    cout << sizeof(int) << endl;
+
+    // sizes of variable pointers
+}
