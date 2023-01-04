7db02d157385a6cb5820888fc7cc84210b1d0e6e
dougsuh <38517374+dougsuh@users.noreply.github.com>
2023-01-04 21:50:19 +0900

    Update lab2_0.cpp

--- lab2/lab2_0.cpp
+++ lab2/lab2_0.cpp
@@ -1,28 +1,15 @@
-#include <fstream>
+// 1 int = 2 shorts
+#include <iostream>
 using namespace std;
 void main() {
-		ofstream moon("jaein.txt");
-		for (int i = 1; i < 10; i++) {
-			moon << "9 X " << i << " = " << i * 9 << endl;
-		}
-		moon.close();
-
-	/*int i, j, k;
-	i = 10, j = 20;
-	cout << " i++ " << i++ << " ++j " << ++j << endl;
-	cout << " i " << i << " j " << j << endl;
-	i += 5;      j /= 5;
-	cout << " i +=5  " << i << endl;
-	cout << " j /= 5 " << j << endl;
-	//
-	i = 0xf;  j = 0x3;  //  i = 1111   j = 0011
-	cout << "  i = 0x" << hex << i;
-	cout << "  j = 0x" << hex << j << endl;
-	i &= j;     // 0011
-	cout << "i &= j 0x" << hex << i << endl; 
-	i = 0xf;    i ^= j;    //   1100
-	cout << "i ^= j 0x" << hex << i << endl; */
-
-	getchar();
+	unsigned short a = 0x23a, b = 0x6b0, d = 0xf001;
+	unsigned int c;
+	c = a << 16 | b;
+	cout << hex  << " 0x"<< a << " 0x" << b << endl; 
+	cout << " 0x" << c;
+	
+	c = a << 16 + d;  // + is OK instead of | in this case
+	cout << " 0x"<< a << " 0x" << d << endl; 
+	cout << " 0x"<< c << " 0x" << d << endl; 
 }
 
