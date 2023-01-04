4839caf44a03502eb607e503c61b028b403ea983
dougsuh <38517374+dougsuh@users.noreply.github.com>
2023-01-04 22:24:18 +0900

    Add files via upload

---
+++ lab2/lab2tfextfile.cpp
@@ -0,0 +1,19 @@
+#include <iostream> // cout cin
+#include <fstream>  // ifstream ofstream
+using namespace std;
+int main() {
+	ofstream seoul("pusan.txt");
+	if (!seoul) {
+		cout << " cant open pusan.txt\n";
+	}
+	int age;
+	float height, weight;
+	age = 22;
+	height = 172.0;
+	weight = 70.5;
+	seoul << age << " years old\n";
+	seoul << height << "cm\n";
+	seoul << weight << "kg\n";
+	seoul.close();
+	return 1;
+}
