2e72feb5de76d1f58804a3e79bf7935a1e008909
dougsuh <38517374+dougsuh@users.noreply.github.com>
2023-01-04 21:54:53 +0900

    Update lab2_0.cpp

--- lab2/lab2_0.cpp
+++ lab2/lab2_0.cpp
@@ -5,11 +5,10 @@ void main() {
 	unsigned short a = 0x23a, b = 0x6b0, d = 0xf001;
 	unsigned int c;
 	c = a << 16 | b;
-	cout << hex  << " 0x"<< a << " 0x" << b << endl; 
-	cout << " 0x" << c;
-	
-	c = a << 16 + d;  // + is OK instead of | in this case
-	cout << " 0x"<< a << " 0x" << d << endl; 
-	cout << " 0x"<< c << " 0x" << d << endl; 
-}
+	cout << hex << " 0x" << a << " 0x" << b << endl;
+	cout << " 0x" << c << endl << endl;
 
+	c = a << 16 | d;  // + is OK instead of | in this case
+	cout << " 0x" << a << " 0x" << d << endl;
+	cout << " 0x" << c << dec << "  " << d << endl;
+}
