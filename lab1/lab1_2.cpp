5f7d1105916ff7f1a6aca77d6759779e607b65de
dougsuh <38517374+dougsuh@users.noreply.github.com>
2023-01-04 17:59:08 +0900

    Add files via upload

---
+++ lab1/lab1_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>      // for cout, cin
+using namespace std;    // for the names used by C++ (include, main, cout, endl…) 
+void main() {
+    int ii;
+    cout << " enter an integer : " << endl;
+    cin >> ii;
+    cout << " 5 * " << ii << " = " << 5 * ii << endl;
+}
