1583f5e541ef9cbbe23a679beaa1921c6136312f
dougsuh <38517374+dougsuh@users.noreply.github.com>
2023-01-06 07:33:43 +0900

    Update lab3_3.cpp

--- lab3/lab3_3.cpp
+++ lab3/lab3_3.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 void main() {
-#include <iostream>
 		int i;
 		short *sa;
 
@@ -15,14 +14,4 @@ void main() {
 		cout << "   i = 0x" << hex << i << endl;
 		cout << "   sa[0] = 0x" << hex << sa[0] << endl;
 		cout << "   sa[1] = 0x" << hex << sa[1] << endl;
-
-	/*
-	int sum[10],N;
-	short root[10];
-	for (int i = 0; i < 10; i++) {
-		cout << "   0x"<< hex <<sum+i <<
-			"   0x" << hex << root+i << endl;
-	}*/
- 	getchar();
 }
-
