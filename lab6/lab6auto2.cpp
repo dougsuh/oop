a4b58dda5bd62f2de01d485631da62240313fc60
dougsuh <38517374+dougsuh@users.noreply.github.com>
2023-01-09 04:32:01 +0900

    auto++

---
+++ lab6/lab6auto2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a[4] = { 100,200,300,400 };
+	float b[3] = { 11.2, 0.5, 0.3 };
+	for (auto c : a) {
+		for (auto d : b) { 
+			cout << c << " + " << d;
+			cout << " = " << c+d << endl; 
+		}
+	}
+}
