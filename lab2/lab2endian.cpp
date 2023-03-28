25ece98a081696b967ec1b9c9498d4c489d3c3f7
dougsuh <38517374+dougsuh@users.noreply.github.com>
2023-03-28 19:23:31 +0900

    Update lab2endian.cpp

--- lab2/lab2endian.cpp
+++ lab2/lab2endian.cpp
@@ -6,6 +6,10 @@ void main() {
 	cout << hex << "0x" <<  ii << endl;
 	char* c;
 	c = (char*)&ii;
-	for (int i = 0; i < 4; i++) cout << (int)c[i] << " ";
+	for (int i = 0; i < 4; i++) {
+		int jj = (int)c[i]; 
+		if (jj < 0) jj += 0x100;  // 2's complement
+		cout << jj << " ";
+	}
 	cout << endl;
 }
