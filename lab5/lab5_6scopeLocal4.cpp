5d4121b4c31f086901c412ee1502d03a54c3cc4a
dougsuh <38517374+dougsuh@users.noreply.github.com>
2023-08-15 12:24:14 +0900

    Add files via upload

---
+++ lab5/lab5_6scopeLocal4.cpp
@@ -0,0 +1,12 @@
+// local variable
+#include <iostream>
+using namespace std;
+void main() {
+	int n = 2;
+	cout << n << "  A\n";
+	for (int n = 0; n < 5; n++) {
+		cout << n << endl;
+	}
+	cout << n << "  B\n";
+}
+
