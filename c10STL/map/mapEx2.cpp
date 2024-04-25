37b49f6a295f35a9bac46c87a5e3e65300eda1ff
dougsuh <38517374+dougsuh@users.noreply.github.com>
2024-04-25 17:20:29 +0900

    Update mapEx2.cpp

--- c10STL/map/mapEx2.cpp
+++ c10STL/map/mapEx2.cpp
@@ -4,39 +4,39 @@
 #include <map>
 
 int main() {
-    // std::map 선언: 문자열 키와 정수 값의 연관 관계를 저장
+    //  std::map : stores (student number, store) as (key, value)
     std::map<int, int> studentScores;
 
-    // 키-값 쌍 추가
+    // input of (student name, score) pairs
     studentScores[10] = 87;
     studentScores[20] = 92;
     studentScores[33] = 78;
     studentScores[25] = 88;
     studentScores[17] = 95;
 
-    // 키를 사용하여 값을 검색
+    // find a value by using a key
     int ii = 17;
     if (studentScores.find(ii) != studentScores.end()) {
-        std::cout << ii << "의 점수: " << studentScores[ii] << std::endl;
+        std::cout << ii << " score : " << studentScores[ii] << std::endl;
     }
     else {
-        std::cout << ii << "의 점수를 찾을 수 없습니다." << std::endl;
+        std::cout << ii << " can not be found."  << std::endl;
     }
-    // 항목을 제거 : "Bob"을 제거
+    //  remove a pair : Student number 33 is to be removed
     studentScores.erase(33);
     ii = 33;
     if (studentScores.find(ii) != studentScores.end()) {
-        std::cout << ii << "의 점수: " << studentScores[ii] << std::endl;
+        std::cout << ii << " score : " << studentScores[ii] << std::endl;
     }
     else {
-        std::cout << ii << "의 점수를 찾을 수 없습니다." << std::endl;
+        std::cout << ii << " can not be found."  << std::endl;
     }
 
-    // 모든 키-값 쌍 순회
-    std::cout << "모든 학생의 성적:" << std::endl;
+    //  visit all pairs
+    std::cout << " scores of all students:"  << std::endl;
     for (const auto& pair : studentScores) {
         std::cout << pair.first << ": " << pair.second << std::endl;
     }
 
-    return 0;
+    return 123;
 }
